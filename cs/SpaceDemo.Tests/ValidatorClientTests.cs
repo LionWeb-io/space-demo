b7e2058731e83d348422d87d58e85f9f073eb8f7
Niko Stotz <niko@nikostotz.de>
2025-10-08 18:09:33 +0200

    more tests

--- cs/SpaceDemo.Tests/ValidatorClientTests.cs
+++ cs/SpaceDemo.Tests/ValidatorClientTests.cs
@@ -44,6 +44,45 @@ public class ValidatorClientTests
         }, source.GetAnnotations().First());
     }
 
+    [TestMethod]
+    public void PeakPowerFixed()
+    {
+        var source = new PowerSource("ps")
+        {
+            Peak = 100
+        };
+
+        var finding = new Finding("f")
+        {
+            Code = 1,
+            Severity = Severity.warning,
+            Message = "Total peak of consumers (110) exceeds source peak"
+        };
+        source.AddAnnotations([finding]);
+        
+        var powerModule = new PowerModule("pm")
+        {
+            Contents =
+            [
+                source,
+                new PowerConsumer("c0")
+                {
+                    Peak = 50,
+                    ProvidedFrom = [source]
+                },
+                new PowerConsumer("c1")
+                {
+                    Peak = 30,
+                    ProvidedFrom = [source]
+                },
+            ]
+        };
+
+        var valid = new Validator.Validator().Validate(powerModule);
+        Assert.IsTrue(valid);
+        Assert.IsEmpty(source.GetAnnotations());
+    }
+
     private static void AssertEquals(IReadableNode expected, IReadableNode actual)
     {
         var differences = new Comparer([expected], [actual]).Compare().ToList();
