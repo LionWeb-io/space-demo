1f0a5c917121f460af9c6fb7fb1c5edfc460dd0f
Ulyana Tikhonova <ulyana@f1re.io>
2025-10-08 13:33:36 +0200

    Projection of power module contents with peak input fields

--- ts/packages/editor/src/projection.tsx
+++ ts/packages/editor/src/projection.tsx
@@ -1,8 +1,9 @@
 import { INodeBase } from "@lionweb/class-core"
 import { observer } from "mobx-react"
 import React from "react"
+import { FaPlug, FaBatteryFull, FaBatteryHalf } from "react-icons/fa"
 
-import { PowerModule } from "./gen/PowerBudget.g.js"
+import { PowerModule, PowerConsumer, PowerSource } from "./gen/PowerBudget.g.js"
 
 
 export const Projection = observer(({ node }: { node: INodeBase }) => {
@@ -10,9 +11,31 @@ export const Projection = observer(({ node }: { node: INodeBase }) => {
     if (node === undefined) {
         return <span>model is empty</span>
     }
+
     if (node instanceof PowerModule) {
-        return <span> power module {node.name} </span>
+    return (
+    <div> 
+        power module {node.name} 
+        { node.contents && node.contents.length > 0  && (
+            <ul>
+                { node.contents.map( (child) => (
+                    <li key = {child.id}>
+                        <Projection node = {child} />
+                    </li> )) }
+            </ul>                
+        )}
+    </div>
+    )
+}
+
+    if (node instanceof PowerConsumer) {
+        return <div> <FaPlug/> power consumer {node.name} with peak <input type ="number" value = {node.peak} onChange = {(e) => {node.peak = parseInt(e.target.value)}} /></div>
     }
+
+    if (node instanceof PowerSource) {
+        return <div> <FaBatteryHalf/> power source {node.name} with peak <input type ="number" value = {node.peak} onChange = {(e) => {node.peak = parseInt(e.target.value)}} </div>
+    }
+
 
     return <span>No projection defined for node of classifier "{node.classifier.name}"</span>
 
