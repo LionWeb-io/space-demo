97accc4288ebeed3d0334e5e3445db4a39aa4081
Meinte Boersma <meinte.boersma@gmail.com>
2026-09-18 22:57:31 +0200

    bump LW TS to 0.10.0 (official)
      + fix some problems after upgrading

--- ts/packages/build/src/generate.ts
+++ ts/packages/build/src/generate.ts
@@ -1,9 +1,10 @@
 import { writeFileSync } from "node:fs"
 import { generateApiFromLanguages } from "@lionweb/class-core-generator"
+import { LionWebVersions } from "@lionweb/core"
 import { deserializeLanguagesWithIoLionWebMpsSpecificFrom, repairIoLionWebMpsSpecificAnnotations } from "@lionweb/io-lionweb-mps-specific"
 import { LionWebJsonChunk } from "@lionweb/json"
-import { generatePlantUmlForLanguage, genericAsTreeText, languagesAsText } from "@lionweb/utilities"
 import { readFileAsJsonSync } from "@lionweb/node-utils"
+import { generatePlantUmlForLanguage, genericAsTreeText, languagesAsText } from "@lionweb/utilities"
 
 
 const languagesJson = readFileAsJsonSync("../../../chunks/space.languages.json") as LionWebJsonChunk
@@ -16,7 +17,7 @@ spaceLanguages.forEach((language) => {
     writeFileSync(`artifacts/${language.name}.puml`, generatePlantUmlForLanguage(language))
 })
 
-generateApiFromLanguages(spaceLanguages, "../react-client/src/gen")
+generateApiFromLanguages(spaceLanguages, "../react-client/src/gen", LionWebVersions.v2023_1)
 
 
 const modelJson = readFileAsJsonSync("../../../chunks/voyager1.instance.json") as LionWebJsonChunk
